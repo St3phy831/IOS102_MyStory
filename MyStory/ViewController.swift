1d62dbc39695d46566ff02e11e67db2379314bff
Stephanie Hernandez <stephernandez831@gmail.com>
2023-01-31 14:34:13 -0800

    Added detail view and set up aboutBtn

--- MyStory/ViewController.swift
+++ MyStory/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aboutMeBtn: UIButton!
+    @IBOutlet weak var abilitiesBtn: UIButton!
+    @IBOutlet weak var funFactBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func aboutBtnWasTapped(_ sender: UIButton) {
+        performSegue(withIdentifier: "detailSegue", sender: sender)
+    }
+    
 }
 
