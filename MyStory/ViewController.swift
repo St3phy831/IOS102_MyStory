26a5a2cb479e2afb3ab82ae3cf016058de852b9e
Stephanie Hernandez <stephernandez831@gmail.com>
2023-01-31 12:24:09 -0800

    Initial Commit

---
+++ MyStory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStory
+//
+//  Created by Stephanie Hernandez on 1/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
