24ab04cc0807c1d9e5f541a02e997663f13ea363
Stephanie Hernandez <stephernandez831@gmail.com>
2023-02-04 15:36:00 -0800

    Created ContextItem

---
+++ MyStory/ContextItem.swift
@@ -0,0 +1,15 @@
+//
+//  ContextItem.swift
+//  MyStory
+//
+//  Created by Stephanie Hernandez on 2/4/23.
+//
+
+import UIKit
+
+// This struct will model the properties for a ContextItem
+struct ContextItem {
+    let title: String
+    let image: UIImage
+    let text: String
+}
